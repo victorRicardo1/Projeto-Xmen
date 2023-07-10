6e68db4092f36e545558ed5f78c1bd695d8d9e13
Ricardo Victor <ricardovictor737@gmail.com>
2023-07-10 20:47:29 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,10 @@ personagens.forEach(personagem => {
    personagem.addEventListener('mouseenter', () => {
         const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado');
-        personagem.classList.add('selecionado');        
-   })
+        personagem.classList.add('selecionado');  
+        
+        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+        imagemPersonagemGrande.src = "./src/imagem/card-vampira.png";
+    
+    })
 }) 
