46a55162432a0d802d226fe489c56a6097fe883b
Ricardo Victor <ricardovictor737@gmail.com>
2023-07-11 20:24:38 -0300

    Finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,40 @@ const personagens = document.querySelectorAll('.personagem');
 
 personagens.forEach(personagem => {
    personagem.addEventListener('mouseenter', () => {
-        const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado');
+
+        if(window.innerWidth < 450){
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
+        removerSelecaoPerson();
+
         personagem.classList.add('selecionado');  
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-        const idPersonagem = personagem.attributes.id.value;
-        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`
-    
+        
+        alterarImagemPersonagemSelect(personagem);
+
+        mudarNomePersonagemSelecionado(personagem);
+
+        mudarDescricaoPersonagemSelecionado(personagem);
     })
 }) 
+
+
+function mudarDescricaoPersonagemSelecionado(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function mudarNomePersonagemSelecionado(personagem) {
+    const NomePersonagem = document.getElementById('nome-personagem');
+    NomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelect(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoPerson() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
